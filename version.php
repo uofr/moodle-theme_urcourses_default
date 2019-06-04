70f1805e696e7557352f31add3e8cb935c5960cf
Trevor Cunningham <trevor.cunningham@uregina.ca>
2019-06-04 15:35:07 -0600

    change version for 3.4 compatibility

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_urcourses_default';
 $plugin->version = 2019051602;
 $plugin->release = 'v3.6-r2';
-$plugin->requires = 2018120300;
+$plugin->requires = 2017111306;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('theme_boost' => 2018120300);
+$plugin->dependencies = array('theme_boost' => 2017111300);
