1ca97aac0138dc48be736340af7c1f6e61dbb53b
JohnOLane <46030891+JohnOLane@users.noreply.github.com>
2020-06-15 14:10:45 -0600

    Modal is only created when user clicks on button. Topic list is only created once instead of being loaded every time the user opens the modal.

--- amd/src/modal_help_ajax.js
+++ amd/src/modal_help_ajax.js
@@ -1,24 +1,30 @@
 define(
 [
-    'core/ajax',
+    'core/ajax'
 ],
 function(
     Ajax
 ){
-	var getModalContent = function() {
-		return Ajax.call([
-			{
-				methodname: 'theme_urcourses_default_get_remtl_help',
-				args: {}
-			},
-			{
-				methodname: 'theme_urcourses_default_get_topic_list',
-				args: {}
-			}
-		]);
-	};
+    var getTopicList = function() {
+        return Ajax.call([
+            {
+                methodname: 'theme_urcourses_default_get_topic_list',
+                args: {}
+            }
+        ])[0];
+    };
+
+    var getRemtlHelp = function() {
+        return Ajax.call([
+            {
+                methodname: 'theme_urcourses_default_get_remtl_help',
+                args: {}
+            }
+        ])[0];
+    };
 
     return {
-		getModalContent: getModalContent
+        getTopicList: getTopicList,
+        getRemtlHelp: getRemtlHelp
     };
 });
