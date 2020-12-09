82ed714b134ef1799297b626a346966611851908
btc989 <bclar787@gmail.com>
2020-12-09 08:11:57 -0600

    Fix to stop errors when no template category exists

--- locallib.php
+++ locallib.php
@@ -382,13 +382,17 @@ function theme_urcourses_default_get_course_templates() {
     //get category for Template Course
     $sql = "SELECT * FROM mdl_course_categories WHERE name = 'TEMPLATES' OR name = 'Template';";
     $category = $DB->get_record_sql($sql, null, IGNORE_MISSING);
+	
+    if (!(array)$category || !$category ){
+        return 0;
+    }
 
     //else use category id to find all courses in the templates category
     $sql = "SELECT * FROM mdl_course WHERE category = {$category->id};";
     $courses = $DB->get_records_sql($sql);
   
     //if no Template category
-    if (!(array)$category || !$category || !(array)$courses) {
+    if (!(array)$courses) {
         return 0;
     }
     
