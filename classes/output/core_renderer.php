339315ebcf73f0aa5843405c49339e80c48066c0
Trevor Cunningham <trevor.cunningham@uregina.ca>
2023-06-14 00:59:21 -0600

    render login from current theme

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -84,7 +84,28 @@ class core_renderer extends \theme_urcourses_default\output\core_renderer {
         return $OUTPUT->image_url('logo', 'theme_uofr_conservatory');
     }
 	
-	
+	/**
+     * Renders the login form.
+     *
+     * @param \core_auth\output\login $form The renderable.
+     * @return string
+     */
+    public function render_login(\core_auth\output\login $form) {
+        global $CFG, $SITE;
+
+        $context = $form->export_for_template($this);
+
+        $context->errorformatted = $this->error_text($context->error);
+        $url = $this->get_logo_url();
+        if ($url) {
+            $url = $url->out(false);
+        }
+        $context->logourl = $url;
+        $context->sitename = format_string($SITE->fullname, true,
+				['context' => context_course::instance(SITEID), "escape" => false]);
+
+        return $this->render_from_template('theme_uofr_conservatory/loginform', $context);
+	}
 }
 
 
