a1a7becb504b41852c8574963fff15538e4fb290
John Lane <46030891+JohnOLane@users.noreply.github.com>
2023-11-01 12:48:31 -0600

    Removing background image from forgot password page.

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -106,9 +106,57 @@ class core_renderer extends \theme_urcourses_default\output\core_renderer {
 
         return $this->render_from_template('theme_uofr_conservatory/loginform', $context);
 	}
+
+    /**
+     * Returns HTML attributes to use within the body tag. This includes an ID and classes.
+     *
+     * This renderer function is copied and modified from /lib/outputrenderers.php
+     *
+     * @since Moodle 2.5.1 2.6
+     * @param string|array $additionalclasses Any additional classes to give the body tag,
+     * @return string
+     */
+    public function body_attributes($additionalclasses = array()) {
+        global $CFG;
+
+        // Require local libraries.
+        require_once($CFG->dirroot . '/theme/boost_union/locallib.php');
+        require_once($CFG->dirroot . '/theme/boost_union/flavours/flavourslib.php');
+
+        if (!is_array($additionalclasses)) {
+            $additionalclasses = explode(' ', $additionalclasses);
+        }
+
+        // If this isn't the login page and the page has a background image, add a class to the body attributes.
+        if ($this->page->pagelayout != 'login') {
+            if (!empty(get_config('theme_boost_union', 'backgroundimage'))) {
+                $additionalclasses[] = 'backgroundimage';
+            }
+        }
+
+        // If this is the login page and the page has a login background image, add a class to the body attributes.
+        if ($this->page->pagelayout == 'login' && $this->page->bodyid != 'page-login-forgot_password') {
+            // Generate the background image class for displaying a random image for the login page.
+            $loginimageclass = theme_boost_union_get_random_loginbackgroundimage_class();
+
+            // If the background image class was returned, we can expect that a background image was set.
+            // In this case, add both the general loginbackgroundimage class as well as the generated
+            // class to the body tag.
+            if ($loginimageclass != '') {
+                $additionalclasses[] = 'loginbackgroundimage';
+                $additionalclasses[] = $loginimageclass;
+            }
+        }
+
+        // If there is a flavour applied to this page, add the flavour ID as additional body class.
+        // Boost Union itself does not need this class for applying the flavour to the page (yet).
+        // However, theme designers might want to use it.
+        $flavour = theme_boost_union_get_flavour_which_applies();
+        if ($flavour != null) {
+            $additionalclasses[] = 'flavour'.'-'.$flavour->id;
+        }
+
+        return ' id="'. $this->body_id().'" class="'.$this->body_css_classes($additionalclasses).'"';
+    }
+
 }
-
-
-
-
-
