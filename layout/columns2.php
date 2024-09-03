ff6828295575b752d0195da7f39b7388a79a97e1
Trevor Cunningham <trevor.cunningham@uregina.ca>
2024-09-02 20:00:36 -0600

    reoroganize darkmode check and add ur-dark-mode body class if enabled

--- layout/columns2.php
+++ layout/columns2.php
@@ -45,33 +45,6 @@ if ($activitynavigation == THEME_BOOST_UNION_SETTING_SELECT_YES) {
     $PAGE->theme->usescourseindex = false;
 }
 
-// Add block button in editing mode.
-$addblockbutton = $OUTPUT->addblockbutton();
-
-$extraclasses = [];
-$bodyattributes = $OUTPUT->body_attributes($extraclasses);
-$blockshtml = $OUTPUT->blocks('side-pre');
-$hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
-
-$secondarynavigation = false;
-$overflow = '';
-if ($PAGE->has_secondary_navigation()) {
-    $tablistnav = $PAGE->has_tablist_secondary_navigation();
-    $moremenu = new \core\navigation\output\more_menu($PAGE->secondarynav, 'nav-tabs', true, $tablistnav);
-    $secondarynavigation = $moremenu->export_for_template($OUTPUT);
-    $overflowdata = $PAGE->secondarynav->get_overflow_menu_data();
-    if (!is_null($overflowdata)) {
-        $overflow = $overflowdata->export_for_template($OUTPUT);
-    }
-}
-
-$primary = new core\navigation\output\primary($PAGE);
-$renderer = $PAGE->get_renderer('core');
-$primarymenu = $primary->export_for_template($renderer);
-$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions()  && !$PAGE->has_secondary_navigation();
-// If the settings menu will be included in the header then don't add it here.
-$regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
-
 //including Dark Mode css if darkmode==1 if query string is set
 //darkmode toggle code
 $setdarkmode = optional_param('darkmode', -1, PARAM_INT);
@@ -99,8 +72,41 @@ if ($setdarkmode > -1) {
     
  }
 
- $darkmodecheck = $DB->get_record('theme_urcourses_darkmode', array('userid'=>$USER->id, 'darkmode'=>1));
- //error_log('darkmode:'.print_r($darkmodecheck,1));
+// Add block button in editing mode.
+$addblockbutton = $OUTPUT->addblockbutton();
+
+$extraclasses = [];
+
+$darkmodecheck = $DB->get_record('theme_urcourses_darkmode', array('userid'=>$USER->id, 'darkmode'=>1));
+//error_log('darkmode:'.print_r($darkmodecheck,1));
+
+if ($darkmodecheck) {
+	$extraclasses[] = 'ur-dark-mode';
+}
+
+$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
+
+$secondarynavigation = false;
+$overflow = '';
+if ($PAGE->has_secondary_navigation()) {
+    $tablistnav = $PAGE->has_tablist_secondary_navigation();
+    $moremenu = new \core\navigation\output\more_menu($PAGE->secondarynav, 'nav-tabs', true, $tablistnav);
+    $secondarynavigation = $moremenu->export_for_template($OUTPUT);
+    $overflowdata = $PAGE->secondarynav->get_overflow_menu_data();
+    if (!is_null($overflowdata)) {
+        $overflow = $overflowdata->export_for_template($OUTPUT);
+    }
+}
+
+$primary = new core\navigation\output\primary($PAGE);
+$renderer = $PAGE->get_renderer('core');
+$primarymenu = $primary->export_for_template($renderer);
+$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions()  && !$PAGE->has_secondary_navigation();
+// If the settings menu will be included in the header then don't add it here.
+$regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
+
 //check if user has darkmode on in database and include if so
 if($darkmodecheck){
    $PAGE->requires->css('/theme/urcourses_default/style/darkmode.css?v=2024050601');
--- layout/drawers.php
+++ layout/drawers.php
@@ -64,11 +64,45 @@ if (defined('BEHAT_SITE_RUNNING') && get_user_preferences('behat_keep_drawer_clo
     $blockdraweropen = true;
 }
 
+//including Dark Mode css if darkmode==1 if query string is set
+//darkmode toggle code
+$setdarkmode = optional_param('darkmode', -1, PARAM_INT);
+
+if ($setdarkmode > -1) {
+    $userid = $USER->id;
+    $table = 'theme_urcourses_darkmode';
+
+    $newrecord = new stdClass();
+    $newrecord->userid = $userid;
+
+    //database check if user has a record, insert if not
+    if ($record = $DB->get_record($table, array('userid'=>$userid))) {
+     //if has a record, update record to $setdarkmode
+     
+     $newrecord->darkmode = $setdarkmode;
+     $newrecord->id = $record->id;
+     $DB->update_record($table, $newrecord);
+    }
+    else {
+        //create a record
+        $newrecord->darkmode = $setdarkmode;
+        $DB->insert_record($table, $newrecord);
+    }  
+    
+}
+
 $extraclasses = ['uses-drawers'];
 if ($courseindexopen) {
     $extraclasses[] = 'drawer-open-index';
 }
 
+$darkmodecheck = $DB->get_record('theme_urcourses_darkmode', array('userid'=>$USER->id, 'darkmode'=>1));
+//error_log('darkmode:'.print_r($darkmodecheck,1));
+if ($darkmodecheck) {
+	$extraclasses[] = 'ur-dark-mode';
+}
+ 
+ 
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
 if (!$hasblocks) {
@@ -101,33 +135,6 @@ $buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_action
 // If the settings menu will be included in the header then don't add it here.
 $regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
 
-//including Dark Mode css if darkmode==1 if query string is set
-//darkmode toggle code
-$setdarkmode = optional_param('darkmode', -1, PARAM_INT);
-
-if ($setdarkmode > -1) {
-    $userid = $USER->id;
-    $table = 'theme_urcourses_darkmode';
-
-    $newrecord = new stdClass();
-    $newrecord->userid = $userid;
-
-    //database check if user has a record, insert if not
-    if ($record = $DB->get_record($table, array('userid'=>$userid))) {
-     //if has a record, update record to $setdarkmode
-     
-     $newrecord->darkmode = $setdarkmode;
-     $newrecord->id = $record->id;
-     $DB->update_record($table, $newrecord);
-    }
-    else {
-        //create a record
-        $newrecord->darkmode = $setdarkmode;
-        $DB->insert_record($table, $newrecord);
-    }  
-    
-}
-
 // load additional css
 $PAGE->requires->css('/theme/urcourses_default/style/legacy.css?v=2024050201');
 //$PAGE->requires->css('/theme/urcourses_default/style/card.css');
@@ -141,11 +148,10 @@ $PAGE->requires->css('/theme/urcourses_default/style/pullquote.css?v=2024050201'
 $PAGE->requires->css('/theme/urcourses_default/style/c4l.css?v=2024050201');
 
 
- $darkmodecheck = $DB->get_record('theme_urcourses_darkmode', array('userid'=>$USER->id, 'darkmode'=>1));
- //error_log('darkmode:'.print_r($darkmodecheck,1));
-//check if user has darkmode on in database and include if so
+// if user has darkmode on, include the CSS
 if($darkmodecheck){
    $PAGE->requires->css('/theme/urcourses_default/style/darkmode.css?v=2024050601');
+   
 }
 
 $header = $PAGE->activityheader;
